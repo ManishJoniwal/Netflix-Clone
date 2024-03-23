b3ee8e195d757cdcd2601758d55cc5e03bfe2eec
Manish Joniwal <163518387+ManishJoniwal@users.noreply.github.com>
2024-03-23 23:18:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+let ques1 = document.querySelector(".ques1")
+let ques2 = document.querySelector(".ques2")
+let ques3 = document.querySelector(".ques3")
+let ques4 = document.querySelector(".ques4")
+let ques5 = document.querySelector(".ques5")
+let ques6 = document.querySelector(".ques6")
+let plus1 = document.querySelector(".plus1")
+let plus2 = document.querySelector(".plus2")
+let plus3 = document.querySelector(".plus3")
+let plus4 = document.querySelector(".plus4")
+let plus5 = document.querySelector(".plus5")
+let plus6 = document.querySelector(".plus6")
+
+let go1 = () => {
+    if (ques1.style.display != "none") {
+        ques1.style.display = "none";
+    }
+    else {
+        ques1.style.display = "block";
+    }
+}
+let go2 = () => {
+    if (ques2.style.display != "none") {
+        ques2.style.display = "none";
+    }
+    else {
+        ques2.style.display = "block";
+    }
+}
+let go3 = () => {
+    if (ques3.style.display != "none") {
+        ques3.style.display = "none";
+    }
+    else {
+        ques3.style.display = "block";
+    }
+}
+let go4 = () => {
+    if (ques4.style.display != "none") {
+        ques4.style.display = "none";
+    }
+    else {
+        ques4.style.display = "block";
+    }
+}
+let go5 = () => {
+    if (ques5.style.display != "none") {
+        ques5.style.display = "none";
+    }
+    else {
+        ques5.style.display = "block";
+    }
+}
+let go6 = () => {
+    if (ques6.style.display != "none") {
+        ques6.style.display = "none";
+    }
+    else {
+        ques6.style.display = "block";
+    }
+}
